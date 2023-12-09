c240d19b618288cf49f529338cb39527b79e4f4f
Dewlar <insawm@gmail.com>
2023-12-09 23:53:23 +0300

    refactor: js handler for burger page

--- coffee-house/js/main.js
+++ coffee-house/js/main.js
@@ -1,26 +1,14 @@
 const burger = document.querySelector('.burger-button');
-const headerNavigation = document.querySelector('.header__navigation');
-const headerMenuButton = document.querySelector('.header-menu-button');
-const headerLogo = document.querySelector('.header__logo');
-const headerWrapper = document.querySelector('.header__wrapper');
-const body = document.body;
 const navLinks = document.querySelector('.main-navigation');
+const burgerElements = document.querySelectorAll('.head-bg');
 
 burger.addEventListener('click', () => {
-    burger.classList.toggle('burger-checked');
-    headerNavigation.classList.toggle('burger-checked');
-    headerMenuButton.classList.toggle('burger-checked');
-    headerLogo.classList.toggle('burger-checked');
-    headerWrapper.classList.toggle('burger-checked');
-    body.classList.toggle('burger-checked');
+    burgerElements
+        .forEach(e => e.classList.toggle('burger-checked'));
 });
 navLinks.addEventListener('click', (e) => {
     if (e.target.classList.contains('link-animation')) {
-        burger.classList.remove('burger-checked');
-        headerNavigation.classList.remove('burger-checked');
-        headerMenuButton.classList.remove('burger-checked');
-        headerLogo.classList.remove('burger-checked');
-        headerWrapper.classList.remove('burger-checked');
-        body.classList.remove('burger-checked');
+        burgerElements
+            .forEach(e => e.classList.remove('burger-checked'));
     }
 });
