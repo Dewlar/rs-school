df89868ba49d0765c365a29f563605933a8b02eb
Dewlar <insawm@gmail.com>
2023-12-12 05:53:38 +0300

    update: some things

---
+++ coffee-house/js/main.js
@@ -0,0 +1,99 @@
+const burger = document.querySelector('.burger-button');
+const navLinks = document.querySelector('.main-navigation');
+const burgerElements = document.querySelectorAll('.head-bg');
+const switchSlideDelay = 5000;
+const sliderControls = document.querySelectorAll('.control');
+
+
+burger.addEventListener('click', () => {
+    burgerElements
+        .forEach(e => e.classList.toggle('burger-checked'));
+});
+navLinks.addEventListener('click', (e) => {
+    if (e.target.classList.contains('link-animation')) {
+        burgerElements
+            .forEach(e => e.classList.remove('burger-checked'));
+    }
+});
+
+
+// slider
+const sliderFrame = document.querySelector('.slide-frame');
+let sliderShift = 0;
+//slider buttons
+document
+    .querySelector('.slide-wrapper')
+    .addEventListener('click', (e) => {
+        e.target.classList.contains('btn-right') && slideShifter('right');
+        e.target.classList.contains('btn-left') && slideShifter('left');
+    });
+
+
+// delay for first slide change
+let timeout = setTimeout(() => {
+    slideShifter('right');
+}, switchSlideDelay);
+
+
+// show control animation for the first slide
+sliderControls[0].classList.toggle('active');
+
+
+const mediaQuery = window.matchMedia('(max-width: 700px)');
+mediaQuery.addEventListener('change', changeMediaQuery);
+
+
+function changeMediaQuery() {
+    resetSliderOffset();
+}
+
+function resetSliderOffset() {
+    clearTimeout(timeout);
+    sliderControls.forEach(control => control.classList.remove('active'));
+    sliderControls[0].classList.toggle('active');
+    sliderShift = 0;
+    sliderFrame.style.translate = sliderShift + 'px';
+    timeout = setTimeout(() => {
+        slideShifter('right');
+    }, switchSlideDelay);
+}
+
+function slideShifter(direction) {
+    const [firstOffset,
+        secondOffset,
+        thirdOffset,
+        endOffset] = mediaQuery.matches ? [0, -448, -896, -1344] : [0, -580, -1160, -1740];
+
+    clearTimeout(timeout);
+    sliderControls.forEach(control => control.classList.remove('active'));
+
+    if (direction === 'right') {
+        sliderShift = sliderShift + secondOffset > endOffset ? sliderShift + secondOffset : 0;
+    }
+    if (direction === 'left') {
+        sliderShift = sliderShift - secondOffset > 0 ? thirdOffset : sliderShift - secondOffset;
+    }
+    sliderFrame.style.translate = sliderShift + 'px';
+
+    sliderShift === firstOffset && sliderControls[0].classList.toggle('active');
+    sliderShift === secondOffset && sliderControls[1].classList.toggle('active');
+    sliderShift === thirdOffset && sliderControls[2].classList.toggle('active');
+    timeout = setTimeout(() => {
+        slideShifter('right');
+    }, switchSlideDelay);
+}
+
+// swiper for touch screen
+let x1 = null;
+let x2 = null;
+document.querySelector('.slider-content').addEventListener('touchstart', (event) => {
+    x1 = event.changedTouches[0].screenX;
+});
+document.querySelector('.slider-content').addEventListener('touchend', (event) => {
+    x2 = event.changedTouches[0].screenX;
+    handleTouchEnd();
+});
+
+function handleTouchEnd() {
+    x2 - x1 < 0 ? slideShifter('right') : slideShifter('left');
+}
---
+++ coffee-house/js/menu.js
@@ -0,0 +1,118 @@
+// burger menu
+const burger = document.querySelector('.burger-button');
+const navLinks = document.querySelector('.main-navigation');
+const burgerElements = document.querySelectorAll('.head-bg');
+
+burger.addEventListener('click', () => {
+    burgerElements
+        .forEach(e => e.classList.toggle('burger-checked'));
+});
+navLinks.addEventListener('click', (e) => {
+    if (e.target.classList.contains('link-animation')) {
+        burgerElements
+            .forEach(e => e.classList.remove('burger-checked'));
+    }
+});
+//////////////////////////////////////////////////
+let gridItems = [];
+
+window.onload = function () {
+
+    //tabs handler
+    addTabsClickHandler();
+    renderArticlesToDom();
+};
+
+const loadData = async () => {
+    const response = await fetch("./db/products.json");
+    gridItems = await response.json();
+    // console.log(gridItems[0].name);
+};
+// await loadData();
+
+const tabBtn = document.querySelectorAll('.offer__tabs .tab');
+const addTabsClickHandler = () => {
+    tabBtn.forEach(button => button.addEventListener('click', switchTab));
+};
+const switchTab = (e) => {
+    removeActiveTabStyle();
+    e.currentTarget.classList.add('active');
+    e.currentTarget.classList.add('tab-no-clickable');
+};
+const removeActiveTabStyle = () => {
+    tabBtn.forEach(button => {
+        if (button.classList.contains('active')) {
+            button.classList.remove('active');
+            button.classList.remove('tab-no-clickable');
+        }
+    });
+};
+
+const renderArticlesToDom = async () => {
+    await loadData();
+    let previewGrid = getPreviewGrid();
+    generateCards(gridItems).forEach(card => {
+        previewGrid.append(card.generateMenuItem());
+    });
+};
+
+const getPreviewGrid = () => {
+    const gridContainer = document.querySelector('.preview__grid');
+    gridContainer.innerHTML = ''; // todo: remove this when card will be generated
+    return gridContainer;
+};
+
+
+const generateCards = (data) => {
+    let cards = [];
+    data.forEach(card => {
+        // console.log(card.category); // todo: можно разделить на категории
+        cards.push(new MenuCard(card));
+    });
+    return cards;
+};
+
+class MenuCard {
+    constructor({name, description, price, image, category}) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+        this.category = category;
+    }
+
+    // menu card-item generator
+    generateMenuItem() {
+        let template = '';
+        let card = document.createElement('div');
+        card.className = 'card';
+        // card.dataset.id = 1;
+
+        template += `<div class="card__photo">`;
+        template += `<img src="assets/menu/${this.image}" alt="coffee preview">`;
+        template += `</div>`;
+        template += `<div class="card__description">`;
+        template += `<h3>${this.name}</h3>`;
+        template += `<p>${this.description}</p>`;
+        template += `<h3>${this.price}</h3>`;
+        template += `</div>`;
+
+        card.innerHTML = template;
+        return card;
+    }
+}
+
+class Modal {
+    constructor(classes) {
+        // this.classes = classes;
+        // this.modal ='';
+        // this.overlay = '';
+        // this.modalCloseBtn = '';
+        // this.modalContent = '';
+    }
+
+    // buildModal(content) {
+    //     this.overlay = document.createElement('div');
+    //     this.overlay.classList.add('o')
+    // }
+}
