183426c005e1c65f0353c15d08eb333386ac24fb
Andrei Markouski <87931143+Dewlar@users.noreply.github.com>
2024-03-17 00:23:51 +0300

    feat: RSS-PZ-02 Comprehensive Field Validation (#11)
    * refactor: change form style, and improve validate input
    
    * feat: add validation on input and form
    
    * refactor: resized the login form

--- src/app/components/view/login-form/login-form.ts
+++ src/app/components/view/login-form/login-form.ts
@@ -8,23 +8,41 @@ export default class LoginForm {
 
   private readonly loginButton: HTMLButtonElement;
 
-  private readonly errorMessages: HTMLElement;
+  private readonly firstNameErrorMessage: HTMLElement;
+
+  private readonly lastNameErrorMessage: HTMLElement;
 
   constructor() {
-    this.firstNameInput = createElement('input', { attrList: { type: 'text', placeholder: 'First Name' } });
-    this.lastNameInput = createElement('input', { attrList: { type: 'text', placeholder: 'Last Name' } });
+    this.firstNameInput = createElement('input', {
+      classList: ['first-name-input'],
+      attrList: { type: 'text', placeholder: 'First Name', maxlength: '15' },
+    });
+    this.lastNameInput = createElement('input', {
+      classList: ['last-name-input'],
+      attrList: { type: 'text', placeholder: 'Last Name', maxlength: '15' },
+    });
     this.loginButton = createElement('button', {
       classList: ['Login'],
       attrList: { disabled: 'true' },
       textContent: 'Login',
     });
-    this.errorMessages = createElement('div', { classList: ['error-messages'] });
+    this.firstNameErrorMessage = createElement('div', { classList: ['error-messages', 'first-error'] });
+    this.lastNameErrorMessage = createElement('div', { classList: ['error-messages', 'first-error'] });
   }
 
   createLoginForm() {
+    const firstNameContainer = createElement('div', {
+      classList: ['first-name'],
+      childNodeList: [this.firstNameInput, this.firstNameErrorMessage],
+    });
+    const lastNameContainer = createElement('div', {
+      classList: ['last-name'],
+      childNodeList: [this.lastNameInput, this.lastNameErrorMessage],
+    });
+
     const form = createElement('form', {
       classList: ['login-form'],
-      childNodeList: [this.firstNameInput, this.lastNameInput, this.loginButton, this.errorMessages],
+      childNodeList: [firstNameContainer, lastNameContainer, this.loginButton],
     });
     const formWrapper = createElement('div', { classList: ['form-wrapper'], childNodeList: [form] });
 
@@ -33,46 +51,51 @@ export default class LoginForm {
   }
 
   private addEventListeners(): void {
-    this.firstNameInput.addEventListener('input', this.validateForm.bind(this));
-    this.lastNameInput.addEventListener('input', this.validateForm.bind(this));
+    this.firstNameInput.addEventListener('input', () => this.validateForm(this.firstNameInput));
+    this.lastNameInput.addEventListener('input', () => this.validateForm(this.lastNameInput));
     this.loginButton.addEventListener('click', this.handleLogin.bind(this));
   }
 
-  private validateForm(): void {
-    const firstNameValid = this.validateName(this.firstNameInput.value.trim());
-    const lastNameValid = this.validateName(this.lastNameInput.value.trim());
-
-    this.firstNameInput.classList.toggle('invalid', !firstNameValid);
-    this.lastNameInput.classList.toggle('invalid', !lastNameValid);
-
-    this.loginButton.disabled = !firstNameValid || !lastNameValid;
-
-    if (!firstNameValid) {
-      this.showError('First Name is not valid');
+  private validateForm(input: HTMLInputElement): void {
+    const [isInputValid, errorMessage] = this.validateName(input);
+    if (input.classList.contains('first-name-input')) {
+      this.firstNameInput.classList.toggle('invalid', !isInputValid);
+      if (!isInputValid) {
+        this.firstNameErrorMessage.textContent = errorMessage;
+      } else this.firstNameErrorMessage.textContent = '';
     }
-
-    if (!lastNameValid) {
-      this.showError('Last Name is not valid');
+    if (input.classList.contains('last-name-input')) {
+      this.lastNameInput.classList.toggle('invalid', !isInputValid);
+      if (!isInputValid) {
+        this.lastNameErrorMessage.textContent = errorMessage;
+      } else this.lastNameErrorMessage.textContent = '';
     }
+    this.loginButton.disabled =
+      this.firstNameInput.classList.contains('invalid') ||
+      this.lastNameInput.classList.contains('invalid') ||
+      this.firstNameInput.value.length === 0 ||
+      this.lastNameInput.value.length === 0;
   }
 
-  private validateName(name: string): boolean {
-    return /^[a-zA-Z]{3,15}$/.test(name);
+  private validateName(input: HTMLInputElement): [boolean, string] {
+    const name = input.value.trim();
+
+    if (input.value.length === 0) return [false, 'The field is required'];
+    if (!/^[A-Z]/.test(input.value)) return [false, 'First letter capitalize required'];
+    if (/^[A-Z]{2,}/.test(input.value)) return [false, 'One letter capitalized'];
+    if (!/^[A-Z][a-z]*(?:-[a-z]*)?$/.test(input.value)) return [false, 'English letters or a hyphen'];
+
+    if (input.classList.contains('first-name-input')) {
+      if (name.length < 3) return [false, 'Minimum 3 symbols required'];
+    } else if (name.length < 4) return [false, 'Minimum 4 symbols required'];
+    if (!/^[A-Z].*[a-z]$/.test(input.value)) return [false, 'First and last letters required'];
+
+    return [true, ''];
   }
 
   private handleLogin(): void {
     const firstName = this.firstNameInput.value.trim();
     const lastName = this.lastNameInput.value.trim();
-
-    // if (!this.validateName(firstName)) {
-    //   this.showError('First Name is not valid');
-    //   return;
-    // }
-    //
-    // if (!this.validateName(lastName)) {
-    //   this.showError('Last Name is not valid');
-    //   return;
-    // }
 
     const userData = {
       firstName,
@@ -80,9 +103,5 @@ export default class LoginForm {
     };
 
     localStorage.setItem('userData', JSON.stringify(userData));
-  }
-
-  private showError(message: string) {
-    this.errorMessages.textContent = message;
   }
 }
