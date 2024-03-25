75df1e279c6c5038c52317c4f926655ca9c14b3c
Andrei Markouski <insawm@gmail.com>
2024-03-25 18:59:23 +0300

    feat: add start-stop buttons

--- client/src/app/car/carRoad.ts
+++ client/src/app/car/carRoad.ts
@@ -1,4 +1,5 @@
 import { svg, svgCar } from './svg/svg';
+import Button from '../button/button';
 
 export default class CarRoad {
   private readonly container: HTMLDivElement;
@@ -9,14 +10,20 @@ export default class CarRoad {
 
   private readonly finish: HTMLDivElement;
 
+  private readonly start: Button;
+
+  private readonly stop: Button;
+
   constructor(color: string) {
     this.container = document.createElement('div');
     this.road = document.createElement('div');
     this.car = document.createElement('div');
     this.finish = document.createElement('div');
+    this.start = new Button('start', 'start');
+    this.stop = new Button('stop', 'stop');
     this.addAttribute(color);
     this.road.append(this.car, this.finish);
-    this.container.append(this.road);
+    this.container.append(this.start.node, this.stop.node, this.road);
   }
 
   private addAttribute(color: string) {
@@ -26,12 +33,22 @@ export default class CarRoad {
     this.car.innerHTML = svgCar(color);
     this.finish.className = 'finish';
     this.finish.innerHTML = svg();
+    this.stop.disabled();
   }
 
   get getNode() {
     return {
+      start: this.start.node,
+      stop: this.stop.node,
       car: this.car,
       line: this.road,
+    };
+  }
+
+  get getStartStopButton() {
+    return {
+      start: this.start,
+      stop: this.stop,
     };
   }
 
